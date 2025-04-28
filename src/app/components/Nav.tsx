10ac28b53b1e9dcac78b63c08ae5070e282c86e7
Ben Smith <bsmith@Mac.fios-router.home>
2025-04-28 12:31:25 -0400

    Updated nav

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -4,12 +4,22 @@ import React, { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import Image from "next/image";
 import RoundButton from "./content-elements/RoundButton";
+import {
+  BookOpenText,
+  Chrome,
+  CircleHelp,
+  Newspaper,
+  Send,
+  TextSearch,
+} from "lucide-react";
+import { siGooglechrome } from "simple-icons/icons";
 
 type NavItem = {
   title: string;
   href: string;
   dropdown?: {
     title: string;
+    href?: string;
     description?: string;
     icon?: React.ReactNode;
   }[];
@@ -70,54 +80,12 @@ const Nav: React.FC = () => {
 
   const navItems: NavItem[] = [
     {
-      title: "Product",
-      href: "#",
-      dropdown: [
-        {
-          title: "Platform",
-          description: "Rely on smarter data",
-          icon: (
-            <div className="w-5 h-5 text-blue-500 bg-blue-100 p-1 rounded">
-              <svg viewBox="0 0 24 24" fill="currentColor">
-                <rect x="3" y="3" width="18" height="18" rx="2" />
-              </svg>
-            </div>
-          ),
-        },
-        {
-          title: "Wealth",
-          description: "Extensive Private Company, 401K Plan Data",
-          icon: (
-            <div className="w-5 h-5 text-teal-500 bg-teal-100 p-1 rounded">
-              <svg viewBox="0 0 24 24" fill="currentColor">
-                <path d="M12 6v12M6 12h12" />
-              </svg>
-            </div>
-          ),
-        },
-        {
-          title: "Contact",
-          description: "Extensive Private Company, 72m+ People Data",
-          icon: (
-            <div className="w-5 h-5 text-purple-500 bg-purple-100 p-1 rounded">
-              <svg viewBox="0 0 24 24" fill="currentColor">
-                <circle cx="12" cy="12" r="10" />
-              </svg>
-            </div>
-          ),
-        },
-        {
-          title: "API",
-          description: "Managed Data Solutions",
-          icon: (
-            <div className="w-5 h-5 text-yellow-500 bg-yellow-100 p-1 rounded">
-              <svg viewBox="0 0 24 24" fill="currentColor">
-                <path d="M4 4h16v16H4z" />
-              </svg>
-            </div>
-          ),
-        },
-      ],
+      title: "Platform",
+      href: "/product",
+    },
+    {
+      title: "Solutions",
+      href: "/solutions",
     },
     {
       title: "Pricing",
@@ -128,60 +96,74 @@ const Nav: React.FC = () => {
       href: "#",
       dropdown: [
         {
-          title: "The Daily Stack",
+          title: "Insights",
+          href: "/insights",
           icon: (
-            <div className="w-5 h-5 text-orange-500 bg-orange-100 p-1 rounded">
-              <svg viewBox="0 0 24 24" fill="currentColor">
-                <path d="M4 4h16v16H4z" />
-              </svg>
+            <div className=" text-blue-500 bg-blue-100 p-1 rounded">
+              <TextSearch />
             </div>
           ),
         },
         {
-          title: "Insights",
+          title: "The Daily Stack",
+          href: "/daily-stack",
           icon: (
-            <div className="w-5 h-5 text-blue-500 bg-blue-100 p-1 rounded">
-              <svg viewBox="0 0 24 24" fill="currentColor">
-                <circle cx="12" cy="12" r="10" />
-              </svg>
+            <div className=" text-orange-500 bg-orange-100 p-1 rounded">
+              <Newspaper />
             </div>
           ),
         },
+
+        {
+          title: "FAQ",
+          href: "/faq",
+          icon: (
+            <div className="text-green-500 bg-green-100 p-1 rounded">
+              <CircleHelp />
+            </div>
+          ),
+        },
+
         {
           title: "PrivCo Dictionary",
+          href: "/knowledge-bank",
           icon: (
-            <div className="w-5 h-5 text-green-500 bg-green-100 p-1 rounded">
-              <svg viewBox="0 0 24 24" fill="currentColor">
-                <path d="M4 4h16v16H4z" />
-              </svg>
+            <div className="text-green-500 bg-green-100 p-1 rounded">
+              <BookOpenText />
             </div>
           ),
         },
-        {
-          title: "Help Center",
-          icon: (
-            <div className="w-5 h-5 text-yellow-500 bg-yellow-100 p-1 rounded">
-              <svg viewBox="0 0 24 24" fill="currentColor">
-                <circle cx="12" cy="12" r="10" />
-              </svg>
-            </div>
-          ),
-        },
+
         {
           title: "Chrome Extension",
+          href: "/chrome-extension",
           icon: (
-            <div className="w-5 h-5 text-purple-500 bg-purple-100 p-1 rounded">
-              <svg viewBox="0 0 24 24" fill="currentColor">
-                <path d="M12 2L2 12l10 10 10-10z" />
+            <div className=" text-purple-500 bg-purple-100 p-1 rounded">
+              <svg
+                role="img"
+                viewBox="-3 -3 30 30"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="currentColor"
+              >
+                <title>{siGooglechrome.title}</title>
+                <path d={siGooglechrome.path} />
               </svg>
+            </div>
+          ),
+        },
+
+        {
+          title: "Contact",
+          href: "/contact",
+          icon: (
+            <div className=" bg-yellow-100 text-yellow-500 p-1 rounded">
+              <Send />
             </div>
           ),
         },
       ],
-    },
-    {
-      title: "Contact",
-      href: "/contact",
     },
   ];
 
@@ -200,16 +182,16 @@ const Nav: React.FC = () => {
 
   return (
     <nav className="sticky top-0 z-40 bg-[#043873] shadow py-4 border-b border-b-black">
-      <div className="max-w-[1600px] md:px-16 px-4 mx-auto flex items-center justify-between">
+      <div className="max-w-[1600px] lg:px-16 px-4 mx-auto flex items-center justify-between">
         {/* Logo */}
         <div className="flex-shrink-0">
           <a href="/" className="flex items-center">
-            <div className="">
+            <div className="max-w-[160px] w-full h-auto">
               <Image
-                src={"/images/logo.png"}
+                src={"/images/logo.svg"}
                 alt={"privco logo"}
-                width={150}
-                height={100}
+                width={1000}
+                height={1000}
                 className="object-cover"
               />
             </div>
@@ -218,106 +200,120 @@ const Nav: React.FC = () => {
 
         {/* Desktop Navigation */}
         {/* Desktop Navigation */}
-        <div className="hidden md:flex md:items-center md:space-x-8">
-          {navItems.map((item) => (
-            <div
-              key={item.title}
-              className="relative"
-              onMouseEnter={() => handleMouseEnter(item.title)}
-              onMouseLeave={handleMouseLeave}
-            >
-              {item.dropdown ? (
-                <button className="text-white px-2 py-1 rounded-md text-lg font-[700] flex items-center focus:outline-none">
-                  {item.title}
-                  <svg
-                    className={`ml-1 w-4 h-4 transform transition-all ease-in-out duration-300 ${
-                      hoverDropdown === item.title ? "rotate-180" : ""
-                    }`}
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
+        <div className="hidden  lg:flex">
+          <div className="lg:flex lg:space-x-8 lg:items-center">
+            {navItems.map((item) => (
+              <div
+                key={item.title}
+                className="relative"
+                onMouseEnter={() => handleMouseEnter(item.title)}
+                onMouseLeave={handleMouseLeave}
+              >
+                {item.dropdown ? (
+                  <button className="text-white pr-2 py-1 rounded-md text-lg font-[700] flex items-center focus:outline-none">
+                    {item.title}
+                    <svg
+                      className={`ml-1 w-4 h-4 transform transition-all ease-in-out duration-300 ${
+                        hoverDropdown === item.title ? "rotate-180" : ""
+                      }`}
+                      fill="none"
+                      stroke="currentColor"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="3"
+                        d="M19 9l-7 7-7-7"
+                      />
+                    </svg>
+                  </button>
+                ) : (
+                  <motion.a
+                    href={item.href}
+                    className="text-white py-1 rounded-md text-lg font-[700] relative group"
+                    whileHover="hover"
+                    initial="initial"
                   >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="3"
-                      d="M19 9l-7 7-7-7"
+                    {item.title}
+                    <motion.span
+                      className="absolute bottom-0 left-0 w-0 h-[2px] bg-white"
+                      variants={{
+                        initial: { width: "0%" },
+                        hover: { width: "100%" },
+                      }}
+                      transition={{ duration: 0.17 }}
                     />
-                  </svg>
-                </button>
-              ) : (
-                <motion.a
-                  href={item.href}
-                  className="text-white py-1 rounded-md text-lg font-[700] relative group"
-                  whileHover="hover"
-                  initial="initial"
-                >
-                  {item.title}
-                  <motion.span
-                    className="absolute bottom-0 left-0 w-0 h-[2px] bg-white"
-                    variants={{
-                      initial: { width: "0%" },
-                      hover: { width: "100%" },
-                    }}
-                    transition={{ duration: 0.17 }}
-                  />
-                </motion.a>
-              )}
-
-              {/* Dropdown with Framer Motion */}
-              <AnimatePresence>
-                {item.dropdown && hoverDropdown === item.title && (
-                  <motion.div
-                    initial={{ opacity: 0, scale: 0.9 }}
-                    animate={{ opacity: 1, scale: 1 }}
-                    exit={{ opacity: 0, scale: 0.9 }}
-                    transition={{
-                      duration: 0.3,
-
-                      ease: [0, 0.71, 0.2, 1.01],
-                    }}
-                    className="absolute z-50 left-0 mt-0 w-80 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5"
-                  >
-                    <div className="py-2 px-4 space-y-2">
-                      {item.dropdown.map((dropdownItem) => (
-                        <a
-                          key={dropdownItem.title}
-                          href="#"
-                          className="block px-2 py-3 hover:bg-gray-50 rounded-md transition-colors duration-150"
-                        >
-                          <div className="flex items-start">
-                            {dropdownItem.icon && (
-                              <div className="flex-shrink-0 mt-1">
-                                {dropdownItem.icon}
-                              </div>
-                            )}
-                            <div className="ml-3">
-                              <p className="text-sm font-medium text-gray-900">
-                                {dropdownItem.title}
-                              </p>
-                              {dropdownItem.description && (
-                                <p className="text-xs text-gray-500">
-                                  {dropdownItem.description}
-                                </p>
-                              )}
-                            </div>
-                          </div>
-                        </a>
-                      ))}
-                    </div>
-                  </motion.div>
+                  </motion.a>
                 )}
-              </AnimatePresence>
-            </div>
-          ))}
+
+                {/* Dropdown with Framer Motion */}
+                <AnimatePresence>
+                  {item.dropdown && hoverDropdown === item.title && (
+                    <motion.div
+                      initial={{ opacity: 0, scale: 0.9 }}
+                      animate={{ opacity: 1, scale: 1 }}
+                      exit={{ opacity: 0, scale: 0.9 }}
+                      transition={{
+                        duration: 0.3,
+
+                        ease: [0, 0.71, 0.2, 1.01],
+                      }}
+                      className="absolute z-50 left-0 mt-0 w-80 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5"
+                    >
+                      <div className="py-2 px-4 space-y-2">
+                        {item.dropdown.map((dropdownItem) => (
+                          <a
+                            key={dropdownItem.title}
+                            href={dropdownItem.href}
+                            className="block px-2 py-3 hover:bg-gray-50 rounded-md transition-colors duration-150"
+                          >
+                            <div className="flex items-center">
+                              {dropdownItem.icon && (
+                                <div className="flex-shrink-0 mt-1">
+                                  {dropdownItem.icon}
+                                </div>
+                              )}
+                              <div className="ml-3">
+                                <p className="text font-medium text-gray-900">
+                                  {dropdownItem.title}
+                                </p>
+                                {dropdownItem.description && (
+                                  <p className="text-xs text-gray-500">
+                                    {dropdownItem.description}
+                                  </p>
+                                )}
+                              </div>
+                            </div>
+                          </a>
+                        ))}
+                      </div>
+                    </motion.div>
+                  )}
+                </AnimatePresence>
+              </div>
+            ))}
+          </div>
           {/* Right side buttons */}
-          <div className="hidden md:flex items-center space-x-4">
-            <a
-              href="/signin"
-              className="text-white hover:text-blue-600 px-3 py-2 rounded-md text-lg font-[700]"
-            >
-              Sign In
-            </a>
+          <div className="hidden lg:flex items-center space-x-6 ml-3">
+            <div>
+              <motion.a
+                href="/signin"
+                className="text-white py-1 rounded-md text-lg font-[700] relative group"
+                whileHover="hover"
+                initial="initial"
+              >
+                Sign In
+                <motion.span
+                  className="absolute bottom-0 left-0 w-0 h-[2px] bg-white"
+                  variants={{
+                    initial: { width: "0%" },
+                    hover: { width: "100%" },
+                  }}
+                  transition={{ duration: 0.17 }}
+                />
+              </motion.a>
+            </div>
             <RoundButton backgroundColor="#34C759" textColor="#FFFFFF">
               Start Free
             </RoundButton>
@@ -325,7 +321,7 @@ const Nav: React.FC = () => {
         </div>
 
         {/* Mobile menu button */}
-        <div className="md:hidden">
+        <div className="lg:hidden">
           <button
             onClick={toggleMenu}
             className="text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none"
@@ -351,7 +347,7 @@ const Nav: React.FC = () => {
       {/* Mobile sidebar overlay */}
       <AnimatePresence>
         {isOpen && (
-          <div className="md:hidden">
+          <div className="lg:hidden">
             {/* Animated Backdrop */}
             <motion.div
               initial="hidden"
@@ -369,11 +365,11 @@ const Nav: React.FC = () => {
               animate="visible"
               exit="exit"
               variants={sidebarVariants}
-              className="fixed inset-y-0 right-0 w-4/5 bg-white z-50 shadow-xl overflow-y-auto"
+              className="fixed inset-y-0 right-0 w-1/3 bg-white z-50 shadow-xl overflow-y-auto"
             >
-              <div className="p-6 border-b border-gray-200">
+              <div className="p-4 px-6 border-b border-gray-200">
                 <div className="flex items-center justify-between">
-                  <h2 className="text-lg font-medium text-gray-900">Menu</h2>
+                  <h2 className="text-xl font-medium text-gray-900">Menu</h2>
                   <button
                     onClick={toggleMenu}
                     className="p-2 rounded-md text-gray-500 hover:text-gray-600 hover:bg-gray-100"
@@ -403,7 +399,7 @@ const Nav: React.FC = () => {
                       <>
                         <button
                           onClick={() => toggleDropdown(item.title)}
-                          className="w-full flex items-center justify-between px-3 py-2 text-gray-700 hover:bg-gray-100 rounded-md text-base font-medium"
+                          className="w-full flex items-center justify-between px-3 py-1 text-gray-700 hover:bg-gray-100 rounded-md text-lg font-medium"
                         >
                           <span>{item.title}</span>
                           <svg
@@ -435,7 +431,7 @@ const Nav: React.FC = () => {
                                 <a
                                   key={dropdownItem.title}
                                   href="#"
-                                  className="block px-3 py-2 rounded-md hover:bg-gray-100 text-sm font-medium text-gray-700"
+                                  className="block px-3 py-2 rounded-md hover:bg-gray-100 font-medium text-gray-700"
                                 >
                                   <div className="flex items-center">
                                     {dropdownItem.icon && (
@@ -463,7 +459,7 @@ const Nav: React.FC = () => {
                     ) : (
                       <a
                         href={item.href}
-                        className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:bg-gray-100"
+                        className="block px-3 py-2 rounded-md text-lg font-medium text-gray-700 hover:bg-gray-100"
                       >
                         {item.title}
                       </a>
@@ -474,13 +470,13 @@ const Nav: React.FC = () => {
                 <div className="pt-6 mt-6 border-t border-gray-200">
                   <a
                     href="/signin"
-                    className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:bg-gray-100"
+                    className="block px-3 py-2 rounded-md text-lg font-medium text-gray-700 hover:bg-gray-100"
                   >
                     Sign In
                   </a>
                   <a
                     href="/get-started"
-                    className="block w-full text-center px-3 py-2 mt-4 rounded-md text-base font-medium text-white bg-blue-500 hover:bg-blue-600"
+                    className="block w-full text-center px-3 py-2 mt-4 rounded-md text-lg font-medium text-white bg-blue-500 hover:bg-blue-600"
                   >
                     Get Started
                   </a>
